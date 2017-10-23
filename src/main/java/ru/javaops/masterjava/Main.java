015c10a90fc8adb9b77817b1e8f6659526de7129
Igor Merkuryev <imerkuryev@luxoft.com>
2017-10-23 06:40:41 +0300

    HW1-optimize-matrix

--- src/main/java/ru/javaops/masterjava/Main.java
+++ src/main/java/ru/javaops/masterjava/Main.java
@@ -1,5 +1,7 @@
 package ru.javaops.masterjava;
 
+import ru.javaops.masterjava.matrix.MatrixUtil;
+
 /**
  * User: gkislin
  * Date: 05.08.2015
@@ -10,5 +12,27 @@ package ru.javaops.masterjava;
 public class Main {
     public static void main(String[] args) {
         System.out.format("Hello MasterJava!");
+
+        int[][] matrixA = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 5, 2, 1 }
+        };
+
+        int[][] matrixB = {
+                { 2, 2, 7 },
+                { 4, 8, 6 },
+                { 2, 3, 1 }
+        };
+
+        int[][] matrixC = MatrixUtil.singleThreadMultiply(matrixA, matrixB);
+
+        System.out.println();
+        for (int i = 0; i < matrixC.length; i++) {
+            for (int j = 0; j < matrixC[i].length; j++) {
+                System.out.print(matrixC[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
